748a3cd9accbbc920d3e8e2293fd0d862e1af16d
Andre Dias <anndrelluizdiias@gmail.com>
2021-10-18 20:20:12 -0300

    Ajustes

--- 2021/Sample.dpr
+++ 2021/Sample.dpr
@@ -3,14 +3,14 @@ program Sample;
 uses
   Vcl.Forms,
   Presenter.Principal in 'presenter\Presenter.Principal.pas' {frmPrincipal},
-  Presenter.Cidade in 'presenter\cidade\Presenter.Cidade.pas' {frmPresenterCidade},
-  Container.Cidade in 'container\cidade\Container.Cidade.pas' {ContainerCidade: TDataModule},
   Service.Cidade in 'service\Service.Cidade.pas',
   Util.Consts in 'util\Util.Consts.pas',
-  Presenter.Pessoa in 'presenter\pessoa\Presenter.Pessoa.pas' {frmPresenterPessoa},
-  Container.Pessoa in 'container\pessoa\Container.Pessoa.pas' {ContainerPessoa: TDataModule},
   Shared.Container.Base in 'shared\Shared.Container.Base.pas' {ContainerBase: TDataModule},
-  Shared.Presenter.Base in 'shared\Shared.Presenter.Base.pas' {frmPresenterBase};
+  Shared.Presenter.Base in 'shared\Shared.Presenter.Base.pas' {frmPresenterBase},
+  Presenter.Cidade in 'presenter\Presenter.Cidade.pas' {frmPresenterCidade},
+  Presenter.Pessoa in 'presenter\Presenter.Pessoa.pas' {frmPresenterPessoa},
+  Container.Cidade in 'container\Container.Cidade.pas' {ContainerCidade: TDataModule},
+  Container.Pessoa in 'container\Container.Pessoa.pas' {ContainerPessoa: TDataModule};
 
 {$R *.res}
 
